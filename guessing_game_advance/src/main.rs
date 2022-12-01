eeb4f7c63953896dc3cb5359a518ac162f360c24
BTBMan <1498118157@qq.com>
2022-12-01 23:35:48 +0800

    chore: loop
    Signed-off-by: BTBMan <1498118157@qq.com>

--- guessing_game_advance/src/main.rs
+++ guessing_game_advance/src/main.rs
@@ -6,8 +6,6 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let mut guess = String::new();
-
     // 生成一个随机数
     // 调用rand::thread_rng函数提供实际使用的生成器
     // 调用生成器的gen_range方法
@@ -18,20 +16,28 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input the guess!");
+    // loop循环比较 只有猜对或转换guess出错会退出循环
+    loop {
+        println!("Please input the guess!");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        let mut guess = String::new();
 
-    // 将guess转为数字类型
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // 将guess转为数字类型
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        println!("You guessed: {guess}")
     }
-
-    println!("You guessed: {guess}")
 }
