998f23fedb058d8c42bccd261f0b64fb5c8b4a94
BTBMan <1498118157@qq.com>
2022-12-06 22:39:44 +0800

    chore: references and borrowing
    Signed-off-by: BTBMan <1498118157@qq.com>

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -87,6 +87,84 @@ fn main() {
         //     }
         // }
 
-        // 如何不获得所有权并且想使用一个值
+        // {
+        //     // 如何不获得所有权并且想使用一个值
+        //     let s1 = String::from("hello");
+        //     println!("{s1}");
+        //     let s2 = test(&s1); // 引用s1(创建一个指向值s1的引用)
+        //     println!("{s1}"); // 有效
+        //     println!("{s2}");
+        //     s2.push_str("abc"); // error 借用的s2不可变
+
+        //     fn test(x: &String) -> &String {
+        //         x
+        //     }
+        // }
+
+        // {
+        //     // 可变引用
+        //     let mut s1 = String::from("hello");
+        //     println!("{s1}");
+        //     let s2 = test(&mut s1); // 引用s1(创建一个指向值s1的引用) 并且是可修改的
+        //     println!("{s1}"); // error
+        //     println!("{s2}");
+        //     s2.push_str(", world"); // 可以修改可变引用
+        //     println!("{s2}");
+        //     println!("{s1}"); // s1已经被修改
+
+        //     fn test(x: &mut String) -> &mut String {
+        //         x
+        //     }
+        // }
+
+        // {
+        //     let mut s = String::from("hello");
+        //     let r1 = &mut s;
+        //     let r2 = &mut s; // error 不能创建多个可变引用
+
+        //     println!("{},{}", r1, r2);
+        // }
+
+        // {
+        //     let mut s = String::from("hello");
+
+        //     {
+        //         let r2 = &mut s; // ok
+        //         println!("{}", r2);
+        //     }
+
+        //     let r1 = &mut s; // ok
+
+        //     println!("{}", r1,);
+        // }
+
+        // {
+        //     let mut s = String::from("hello");
+        //     let r1 = &s;
+        //     let r2 = &s;
+        //     let r3 = &mut s; // error
+
+        //     println!("{},{},{}", r1, r2, r3);
+        // }
+
+        // {
+        //     let mut s = String::from("hello");
+        //     let r1 = &s;
+        //     let r2 = &s;
+        //     println!("{},{}", r1, r2);
+        //     let r3 = &mut s; // ok, 因为再次之后r1 r2不再使用
+
+        //     println!("{}", r3);
+        // }
+
+        // {
+        //     // 悬垂引用
+        //     fn dangle() -> &String {
+        //         // error
+        //         let s = String::from("hello");
+
+        //         &s // 返回s的引用
+        //     } // s离开作用域, 内存被释放, 这时候会形成悬垂引用
+        // }
     }
 }
