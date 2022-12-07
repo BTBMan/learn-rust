57eed784333634053cfc6d3d8ce32ec170c3893b
BTBMan <1498118157@qq.com>
2022-12-07 11:32:15 +0800

    chore: slice
    Signed-off-by: BTBMan <1498118157@qq.com>

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -166,5 +166,103 @@ fn main() {
         //         &s // 返回s的引用
         //     } // s离开作用域, 内存被释放, 这时候会形成悬垂引用
         // }
+
+        // {
+        //     let mut s = String::from("hello world");
+        //     let w = first_word(&s);
+
+        //     println!("{w}"); // 5
+        //     s.clear(); // 清空了字符串 为""
+        //     println!("{w}"); // 此时还是为5
+
+        //     fn first_word(s: &String) -> usize {
+        //         let bytes = s.as_bytes();
+
+        //         for (i, &item) in bytes.iter().enumerate() {
+        //             if item == b' ' {
+        //                 return i;
+        //             }
+        //         }
+
+        //         s.len()
+        //     }
+        // }
+
+        // {
+        //     // slice
+        //     let s = String::from("hello world");
+        //     let w = first_word(&s);
+
+        //     println!("{w}"); // hello
+        //     s.clear(); // error 使用了slice会确保String的引用持续有效
+        //     println!("{w}"); // hello
+
+        //     fn first_word(s: &String) -> &str {
+        //         let bytes = s.as_bytes();
+
+        //         for (i, &item) in bytes.iter().enumerate() {
+        //             if item == b' ' {
+        //                 return &s[0..i];
+        //             }
+        //         }
+
+        //         &s[..]
+        //     }
+        // }
+
+        // {
+        //     let s = "hello"; // 双引号的字符串是一个不可变引用
+        // }
+
+        // {
+        //     // slice
+        //     let s1 = String::from("hello world");
+        //     let w1 = first_word(&s1);
+        //     let w2 = first_word(&s1[..]); // ok 得益于函数接收参数的类型是一个字符串slice
+        //     let w3 = first_word(&s1[0..6]);
+
+        //     println!("{w1}"); // hello
+        //     println!("{w2}"); // hello
+        //     println!("{w3}"); // hello
+
+        //     // all works!
+        //     let s2 = "hello world";
+        //     let w4 = first_word(&s2);
+        //     let w5 = first_word(&s2[..]);
+        //     let w6 = first_word(&s2[0..6]);
+        //     let w7 = first_word(s2);
+
+        //     let s3 = "hello world";
+        //     let s4 = s3;
+
+        //     println!("{s3}");
+        //     println!("{s4}");
+
+        //     println!("{w4}"); // hello
+        //     println!("{w5}"); // hello
+        //     println!("{w6}"); // hello
+        //     println!("{w7}"); // hello
+
+        //     // => different
+        //     fn first_word(s: &str) -> &str {
+        //         let bytes = s.as_bytes();
+
+        //         for (i, &item) in bytes.iter().enumerate() {
+        //             if item == b' ' {
+        //                 return &s[0..i];
+        //             }
+        //         }
+
+        //         &s[..]
+        //     }
+        // }
+
+        {
+            // 数组的slice (可以对所有的集合使用这种slice)
+            let a = [1, 2, 3, 4, 5];
+            let slice = &a[0..1];
+
+            println!("{}", { slice[0] }); // 1
+        }
     }
 }
