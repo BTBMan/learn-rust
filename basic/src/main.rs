f8710ad2e0a0ccbee8a8538ef6fd5c97a06e139f
BTBMan <1498118157@qq.com>
2024-03-18 16:16:35 +0800

    wip: wip
    Signed-off-by: BTBMan <1498118157@qq.com>

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -84,4 +84,35 @@ fn main() {
     //     println!("{a},{b}");
     //     assert_eq!(5, *b); // 解引用之后才能访问b所指向的值 然后进行比较. 不解引用的话 b是个引用 不能与整型进行比较
     // }
+    // 字符串拼接
+    // {
+    //     let s1 = String::from("Hello");
+    //     let s2 = String::from("world");
+
+    //     // String 必须和一个字符串切片($str)进行拼接
+    //     let s3 = s1 + &s2;
+
+    //     println!("{s3}");
+    // }
+    // 字符串转义
+    // {
+    //     let s1 = "I'm writing \x52\x75\x73\x74!";
+
+    //     println!("{s1}");
+
+    //     // 保持原样
+    //     println!("{}", "hello \\x52\\x75\\x73\\x74");
+
+    //     let raw_str = r"I'm writing \x52\x75\x73\x74!";
+
+    //     println!("{raw_str}");
+
+    //     let quotes = r#"And then I said: "There is no escape!""#;
+
+    //     println!("{}", quotes);
+
+    //     let longer_delimiter = r###"A string with "# in it. And even "##!"###;
+
+    //     println!("{}", longer_delimiter);
+    // }
 }
