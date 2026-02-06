e8c19848c0902bfdf892c849ad7a48f1eea5981b
JohnWon <1498118157@qq.com>
2026-02-06 21:13:01 +0800

    feat: module
    Signed-off-by: JohnWon <1498118157@qq.com>

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -63,18 +63,85 @@
 // }
 
 // -------------------------------------------------
-mod front_of_house1;
-use crate::front_of_house1::hosting; // 只用有 mod 声明了才可以用 use
 
-mod front_of_house2;
-use crate::front_of_house2::hosting as hosting2;
+// mod front_of_house1;
+// use crate::front_of_house1::hosting; // 只用有 mod 声明了才可以用 use
+// // use front_of_house1::hosting; // 也可以通过相对路径引入
 
-// 引用
-fn eat_at_restaurant() {
-    // 引用
-    crate::front_of_house1::hosting::add_to_waitlist();
-    hosting::add_to_waitlist();
+// // mod front_of_house2;
+// // use crate::front_of_house2::hosting as hosting2;
+// // use front_of_house2::hosting as hosting2;
 
-    crate::front_of_house2::hosting::add_to_waitlist();
-    hosting2::add_to_waitlist();
+// // pub use front_of_house2::hosting as hosting_to_export; // 默认导入引来的模块会自动转为私有的, 如果想再次把这个引用的模块导出供外部使用, 则在前面加上 pub 变为可见的
+
+// // use std::io::{self, Write}; // 通过 self 引入 io 模块自身
+// // use std::io::*; // 通过 * 引入 io 模块中的所有项
+
+// // 引用
+// fn eat_at_restaurant() {
+//     // 引用
+//     crate::front_of_house1::hosting::add_to_waitlist();
+//     hosting::add_to_waitlist();
+
+//     // crate::front_of_house2::hosting::add_to_waitlist();
+//     // hosting2::add_to_waitlist();
+
+//     // io::Write;
+//     // Write;
+// }
+
+// -------------------------------------------------
+
+// 如果我们希望某个 item 在特定的模块中可见
+// 可以使用 pub(in crate::module) 的方式
+pub mod a {
+    pub const I: i32 = 3;
+
+    fn semisecret(x: i32) -> i32 {
+        use self::b::c::J; // 由于 self 是 mod a 即是父模块, 所以不能访问子模块 b 内部的 item
+        // use self::b::d; // error, d 只在 d 当前的作用域可见
+        use self::bb::e;
+        e();
+        use self::bb::f;
+        f();
+        x + J
+    }
+
+    pub fn bar(z: i32) -> i32 {
+        semisecret(I) * z
+    }
+    pub fn foo(y: i32) -> i32 {
+        semisecret(I) + y
+    }
+
+    mod b {
+        // 通过 pub (in crate::xxx) 的方式指定当前 c 只在 a 中可见
+        // xxx 必须是父模块或祖先模块
+        pub(in crate::a) mod c {
+            pub(in crate::a) const J: i32 = 4;
+        }
+    }
+
+    pub mod bb {
+        pub(in crate::a) fn g() {}
+
+        // 使用 pub(self) 定义 item 只在当前模块内可见
+        pub(self) fn d() {
+            e();
+        }
+
+        // 使用 pub(super) 定义的 item 只在父模块可见
+        pub(super) fn e() {
+            d();
+        }
+
+        // 使用 pub(crate) 定义的 item 只能在当前包中可见
+        pub(crate) fn f() {}
+    }
+}
+
+fn test() {
+    // a::bb::e(); // error, e 只能在 mod a 中使用
+    // a::bb::g(); // error, g 只能在 mod a 中使用
+    a::bb::f(); // ok, 可以在同一个 crate 中的任何地方使用, 如果被其他 crate 引用的话, 就访问不到了
 }
