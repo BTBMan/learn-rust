d8f68e7edd15e9055473437cbe3d066c405e79d9
JohnWon <1498118157@qq.com>
2026-02-07 16:39:21 +0800

    refactor: use iterator
    Signed-off-by: JohnWon <1498118157@qq.com>

--- finder/src/lib.rs
+++ finder/src/lib.rs
@@ -1,10 +1,16 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.path)?;
 
-    for line in search(&config.query, &content) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &content)
+    } else {
+        search(&config.query, &content)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -12,32 +18,47 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in content.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
+    content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
 
-    return results;
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    content
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[derive(Debug)]
 pub struct Config {
     query: String,
     path: String,
+    ignore_case: bool,
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("参数个数不够");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next(); // 第一个参数是程序名, 跳过
 
-        let query = args[1].clone();
-        let path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("query 参数获取失败"),
+        };
 
-        Ok(Config { query, path })
+        let path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("path 参数获取失败"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            path,
+            ignore_case,
+        })
     }
 }
 
@@ -54,5 +75,32 @@ safe, fast, productive.
 Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(&query, &content));
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(&query, &content));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(&query, &content)
+        );
     }
 }
--- finder/src/main.rs
+++ finder/src/main.rs
@@ -2,14 +2,14 @@ use finder::*;
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("解析参数错误: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("解析参数错误: {err}");
         process::exit(1);
     });
 
     // 也可以和处理 build 一样使用 unwrap_or_else 处理错误
     if let Err(e) = run(config) {
-        println!("程序错误: {e}");
+        // eprintln 用来重定向到 stderr, 即当我们把输入的内容作为日志输出到文件中, 如果它是 eprintln, 则只会在控制台输出, 并不会输出到文件里
+        eprintln!("程序错误: {e}");
     }
 }
