627b2c49ad3affd31d5339c9130a64b892c7eedc
JohnWon <1498118157@qq.com>
2026-02-22 15:38:56 +0800

    feat: unsafe
    Signed-off-by: JohnWon <1498118157@qq.com>

--- basic/src/unsafe_code.rs
+++ basic/src/unsafe_code.rs
@@ -3,6 +3,9 @@ pub fn main() {
     //     let mut num = 5;
     //     let r1 = &num as *const i32; // 裸指针/原生指针(raw pointer)
 
+    //     println!("{r1:?}"); // 内存地址
+    //     println!("{r2:?}"); // 内存地址
+
     //     // unsafe 绕不开 rust 的借用检查
     //     unsafe {
     //         println!("{}", *r1);
@@ -47,32 +50,62 @@ pub fn main() {
     //     }
     // }
 
-    {
-        let mut v = vec![1, 2, 3, 4, 5, 6];
-        let r = &mut v[..]; // 转换为切片
-                            // let (a, b) = split_at_mut(r, 3);
+    // {
+    //     use std::slice::{self as std_slice};
 
-        let ptr = r.as_mut_ptr();
+    //     let mut v = vec![1, 2, 3, 4, 5, 6];
+    //     let r = &mut v[..]; // 转换为切片
+    //     let (a, b) = split_at_mut(r, 3);
 
-        println!("{:?}", ptr);
+    //     println!("a: {:?}", a);
+    //     println!("b: {:?}", b);
 
-        // fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    //     // fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
 
-        //     let len = slice.len();
-        //     assert!(mid <= len);
+    //     //     let len = slice.len();
+    //     //     assert!(mid <= len);
 
-        //     // 这里正常是会报错的, 因为同时拥有两个可变引用
-        //     (&mut slice[..mid], &mut slice[mid..])
-        // }
+    //     //     // 这里正常是会报错的, 因为同时拥有两个可变引用
+    //     //     (&mut slice[..mid], &mut slice[mid..])
+    //     // }
 
-        // unsafe 版
-        // fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
-        //     let len = slice.len();
-        //     let ptr = slice.as_mut_ptr();
-        //     assert!(mid <= len);
+    //     // unsafe 版
+    //     fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    //         let len = slice.len();
+    //         let ptr = slice.as_mut_ptr(); // 获取切片的裸指针, 也就是第一个元素的地址
+    //         assert!(mid <= len);
 
-        //     // 这里正常是会报错的, 因为同时拥有两个可变引用
-        //     // (&mut slice[..mid], &mut slice[mid..])
-        // }
-    }
+    //         unsafe {
+    //             println!("ptr: {:?}", *ptr); // 切片的第一个元素
+    //         }
+
+    //         unsafe {
+    //             (
+    //                 std_slice::from_raw_parts_mut(ptr, mid),
+    //                 std_slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+    //             )
+    //         }
+    //     }
+    // }
+
+    // // FFI (Foreign Function Interface)
+    // // 可以与其他语言进行交互
+    // {
+    //     // 定义其他语言的代码, 以 C 语言为例
+    //     // 这里的 C 就是 ABI, 除了 C 还有其他的
+    //     extern "C" {
+    //         fn abs(input: i32) -> i32;
+    //     }
+
+    //     unsafe {
+    //         println!("{}", abs(-1));
+    //     }
+
+    //     // 声明一个其他语言可以调用的 rust 函数
+    //     // 需要使用 #[no_mangle] 来避免 rust 编译器对函数名进行混淆
+    //     #[no_mangle]
+    //     pub extern "C" fn add(a: i32, b: i32) -> i32 {
+    //         a + b
+    //     }
+    // }
 }
